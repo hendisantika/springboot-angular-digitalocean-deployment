2bdf8564cd370f4ca06e84ef80185b6e2cb3a4d2
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-23 09:40:29 +0700

    #7 - Add getAllProduct API in Product Controller - Product Service

--- product-service/src/main/java/com/hendisantika/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/hendisantika/productservice/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.hendisantika.productservice.controller;
 
+import com.hendisantika.productservice.entity.Product;
 import com.hendisantika.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +25,8 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    @GetMapping
+    public List<Product> getAllProduct() {
+        return service.getAllProduct();
+    }
 }
