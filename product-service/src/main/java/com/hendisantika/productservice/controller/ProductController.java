2d4426b04efe6b9d9e9ab6e5b3ae84ca9ddd42b4
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-23 09:40:56 +0700

    #8 - Add getProductById API in Product Controller - Product Service

--- product-service/src/main/java/com/hendisantika/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/hendisantika/productservice/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.hendisantika.productservice.entity.Product;
 import com.hendisantika.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,10 @@ public class ProductController {
     @GetMapping
     public List<Product> getAllProduct() {
         return service.getAllProduct();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return service.getOneProduct(id);
     }
 }
