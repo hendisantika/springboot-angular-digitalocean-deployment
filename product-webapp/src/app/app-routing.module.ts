6a8e072fb613a45fb033e3ee1e690ed0e0f18f41
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-25 06:34:13 +0700

    #29 - Set router in Product WebApp Module

--- product-webapp/src/app/app-routing.module.ts
+++ product-webapp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {IndexComponent} from './pages/index/index.component';
+import {CreateProductComponent} from './pages/create-product/create-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent, pathMatch: 'full'},
+  {path: 'add', component: CreateProductComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
