d0ad0e3513485fed7634e628936b5c0fa806b9d6
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-25 06:41:12 +0700

    #38 - Add afterInit in Product WebApp Module

--- product-webapp/src/app/pages/index/index.component.ts
+++ product-webapp/src/app/pages/index/index.component.ts
@@ -1,16 +1,28 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {ProductService} from '../../services/product.service';
+import {Router} from '@angular/router';
+import {Product} from '../../model/product';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
-export class IndexComponent implements OnInit {
+export class IndexComponent implements OnInit, AfterViewInit {
 
-  constructor() {
+  public product: Product;
+
+  constructor(
+    private route: Router,
+    private productService: ProductService
+  ) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.productService.getter();
   }
 
+  ngAfterViewInit() {
+    this.getAllProducts();
+  }
 }
