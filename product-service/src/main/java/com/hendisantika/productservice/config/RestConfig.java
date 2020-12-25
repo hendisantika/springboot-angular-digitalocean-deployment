aa83e31a7f68f76c9b340477c72b63895ac93eb9
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-25 07:04:23 +0700

    #49 - Add RestConfig - Product Service

---
+++ product-service/src/main/java/com/hendisantika/productservice/config/RestConfig.java
@@ -0,0 +1,35 @@
+package com.hendisantika.productservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : product-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 25/12/20
+ * Time: 07.02
+ */
+@Configuration
+public class RestConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
